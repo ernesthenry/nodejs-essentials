e27358f940bc1343449683cd04214258d31a9e63
ernesthenry <henry38ernest@gmail.com>
2020-03-18 00:14:14 +0300

    Add more functionality to the timer

--- timers.js
+++ timers.js
@@ -1,17 +1,23 @@
-const waitTime = 3000;
+const waitTime = 5000;
 const waitInterval = 500;
 let currentTime = 0;
 
 const incrementTime = () => {
     currentTime += waitInterval;
-    console.log(`waiting ${currentTime/1000} seconds`);
-}
+    const p = Math.floor((currentTime/waitTime)*100);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`waiting ........${p}%`)
+ }
+console.log(`waiting ${currentTime/1000} seconds`);
 
 
 console.log(`Setting a ${waitTime/1000} second delay`)
 
 const timerFinished = () => {
-    clearInterval(interval)
+    clearInterval(interval);
+    process.stdout.clearLine(); // clear the line
+    process.stdout.cursorTo(0); // move the cursor back to 0
     console.log('done')
 }
 
