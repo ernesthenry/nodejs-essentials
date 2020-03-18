fc1c2c031676fd7e58532b0d03c180f123ec760c
ernesthenry <henry38ernest@gmail.com>
2020-03-18 13:55:18 +0300

    Wire listeners to the on() function to listen for events emitted

--- events.js
+++ events.js
@@ -2,9 +2,18 @@ const events = require('events')
 
 const emitter = new events.EventEmitter()
 
+// Event emitter gives us a way to raise and handle custom events that we can wire listeners to the on() function
+// event emitter is asynchronous
+
 emitter.on("customEvent", (message, user) =>{
     console.log(`${user}: ${message}`)
 })
 
-emitter.emit("customEvent", "Hello world", "Computer")
-emitter.emit("customEvent", "How are you doing?", "Ernest Henry")
+process.stdin.on("data", data =>{
+    const input = data.toString().trim();
+    if(input === "exit"){
+        emitter.emit("customEvent", "Good bye", "process");
+        process.exit();
+    }
+    emitter.emit("customEvent", input, "terminal")
+}) 
