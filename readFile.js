f744eabf6aac70295cddb05e9061939eae58952a
ernesthenry <henry38ernest@gmail.com>
2020-03-24 23:33:31 +0300

    capture error if any has ocurred

--- readFile.js
+++ readFile.js
@@ -2,7 +2,8 @@ const fs = require("fs")
 
 fs.readFile("./assets/alex.jpg",  (err, img) =>{
     if (err){
-        console.log(`Error has ocurred ${err}`)
+        console.log(`Error has ocurred ${err.message}`)
+        process.exit()
     }
     console.log("File contents read")
     console.log(img)
